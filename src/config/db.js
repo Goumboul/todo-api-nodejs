e5d82127866d7c1a4a58cd1068bff131679a4b19
Goumboul <pachtoune4@gmail.com>
2025-05-27 18:10:27 +0200

    [ADD] db

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2/promise');
+
+console.log('Variables d\'environnement :', {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+});
+
+module.exports = pool;
